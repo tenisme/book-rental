2c3e7ed05f89af22bc1537d1572205caaa6fa841
tenisme <iamchoma@gmail.com>
2020-09-15 16:59:56 +0900

    update 200915-1659

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 // Routes
-app.get('/*', (req, res) => {
+app.get("/*", (req, res) => {
   res.send(`Request received: ${req.method} - ${req.path}`);
 });
 
 // Error handler
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).send('Internal Serverless Error');
+  res.status(500).send("Internal Serverless Error");
 });
+
+app.listen(5700, console.log(`Server running on port 5700`));
 
 module.exports = app;
